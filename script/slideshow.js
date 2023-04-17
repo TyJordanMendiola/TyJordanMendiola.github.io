8660c44113be3df46bd8e5e3322fe78b9fceabd9
Ty Mendiola <TyJordanC.Mendiola@calbaptist.edu>
2023-04-17 09:38:26 -0700

    Updated Code

--- script/slideshow.js
+++ script/slideshow.js
@@ -1,17 +1,1 @@
-var slideshows = document.querySelectorAll('[data-component="slideshow"');
-
-slideshows.forEach(initSlideShow);
-
-function initSlideShow(slideshow) {
-    var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] div.slide`);
-    var index = 0, time = 5000;
-    slides[index].classList.add('active');
-
-    setInterval (() => {
-        slides[index].classList.remove('active');
-        index++;
-
-        if (index == slides.length) index = 0;
-        slides[index].classList.add('active');
-    }, time)
-}
+var info = ["Use the navigation bar or the arrows to navigate through various webpages!", "Expect an organized layout of content for each webpage!"]
